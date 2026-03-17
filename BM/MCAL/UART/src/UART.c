ca2f94e3816c3226cfc1cc679425887079f33475
ESMAIL\s_a_a <esmailqassem@gmail.com>
2026-03-17 12:38:34 +0200

    update the uart driver

--- BM/MCAL/UART/src/UART.c
+++ BM/MCAL/UART/src/UART.c
@@ -1,6 +1,8 @@
 #include "UART.h"
 uint8 rx_test;
-void (*funcPtr)(uint8)=NULL;
+void (*uart1_funcPtr)(uint8)=NULL;
+void (*uart2_funcPtr)(uint8)=NULL;
+void (*uart3_funcPtr)(uint8)=NULL;
 
 uint32 Get_BASE_ADD(uint8 base)
 {
@@ -101,11 +103,25 @@ void UART_voidSendNumber(UART_HardWare_t HardWare_Unit,sint32 Copy_sint32Number)
     Write_UART_Number(HardWare_Unit,Copy_sint32Number);
 }
 
+void UART1_CALLBACK(void(*p2function)(uint8))
+{
+    if(p2function!= NULL)
+    {
+         uart1_funcPtr = p2function;
+    }
+}
 void UART2_CALLBACK(void(*p2function)(uint8))
 {
     if(p2function!= NULL)
     {
-         funcPtr = p2function;
+         uart2_funcPtr = p2function;
+    }
+}
+void UART3_CALLBACK(void(*p2function)(uint8))
+{
+    if(p2function!= NULL)
+    {
+         uart3_funcPtr = p2function;
     }
 }
 
@@ -118,7 +134,7 @@ void USART2_IRQHandler(void)
     if (status & (1 << 5))  // RXNE
     {
         rx_test= (uint8)USART_DR(Add);  // Reading DR clears RXNE
-        funcPtr(rx_test);
+        uart2_funcPtr(rx_test);
     }
     
     /* Overrun Error */
@@ -145,4 +161,83 @@ void USART2_IRQHandler(void)
         volatile uint32 temp = USART_SR(Add);
         (void)temp;
     }
+}
+
+void USART3_IRQHandler(void)
+{
+        uint32 Add = Get_BASE_ADD(UART3);
+    uint32 status = USART_SR(Add);
+    
+    /* RX Data Available */
+    if (status & (1 << 5))  // RXNE
+    {
+        rx_test= (uint8)USART_DR(Add);  // Reading DR clears RXNE
+        uart3_funcPtr(rx_test);
+    }
+    
+    /* Overrun Error */
+    if (status & (1 << 3))  // ORE
+    {
+        // Clear by reading SR then DR
+        volatile uint32 temp = USART_SR(Add);
+        temp = USART_DR(Add);
+        (void)temp;
+    }
+    
+    /* Framing Error */
+    if (status & (1 << 2))  // FE
+    {
+        // Clear by reading SR
+        volatile uint32 temp = USART_SR(Add);
+        (void)temp;
+    }
+    
+    /* Noise Error */
+    if (status & (1 << 1))  // NE  
+    {
+        // Clear by reading SR
+        volatile uint32 temp = USART_SR(Add);
+        (void)temp;
+    }
+
+
+}
+
+void USART1_IRQHandler(void)
+{
+        uint32 Add = Get_BASE_ADD(UART1);
+    uint32 status = USART_SR(Add);
+    
+    /* RX Data Available */
+    if (status & (1 << 5))  // RXNE
+    {
+        rx_test= (uint8)USART_DR(Add);  // Reading DR clears RXNE
+        uart1_funcPtr(rx_test);
+    }
+    
+    /* Overrun Error */
+    if (status & (1 << 3))  // ORE
+    {
+        // Clear by reading SR then DR
+        volatile uint32 temp = USART_SR(Add);
+        temp = USART_DR(Add);
+        (void)temp;
+    }
+    
+    /* Framing Error */
+    if (status & (1 << 2))  // FE
+    {
+        // Clear by reading SR
+        volatile uint32 temp = USART_SR(Add);
+        (void)temp;
+    }
+    
+    /* Noise Error */
+    if (status & (1 << 1))  // NE  
+    {
+        // Clear by reading SR
+        volatile uint32 temp = USART_SR(Add);
+        (void)temp;
+    }    
+
 }
