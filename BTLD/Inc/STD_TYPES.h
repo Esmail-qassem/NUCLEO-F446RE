16ce66cc7987a08c1fdececeec0ffc7db55e7035
Muhammad Ahmad Sayed <55844962+MuhammadSmart@users.noreply.github.com>
2026-04-03 08:42:20 +0200

    btld: guard flash erase, derive clocks, and replace magic numbers
    - Compute APB clocks for UART init with HSI fallback + error banner
    - Erase APP sector only on update session start
    - Add readable macros for UART/flash/addresses/messages
    - Fix volatile linkage for UART_START and ms_ticks
    - Add uint64/sint64 typedefs for bootloader

--- BTLD/Inc/STD_TYPES.h
+++ BTLD/Inc/STD_TYPES.h
@@ -26,6 +26,8 @@ typedef signed long int sint32;
 typedef unsigned char uint8;
 typedef unsigned short int uint16;
 typedef unsigned long int uint32;
+typedef signed long long int sint64;
+typedef unsigned long long int uint64;
 typedef float float32;
 typedef double float64;
 typedef long double float128;
